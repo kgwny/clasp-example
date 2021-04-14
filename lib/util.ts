d4965b6b0afd372f27491fd7a9b8f235ef188dc2
kgwny <kigawanaoya@gmail.com>
2021-04-14 23:51:12 +0900

    add snippet

--- lib/util.ts
+++ lib/util.ts
@@ -4,3 +4,12 @@ export const getDayFormat = (date = new Date()): string => {
   const day = date.getDate();
   return `${year}-${month}-${day}`;
 };
+
+//列番号のアルファベット列名変換
+function wmap_column_convert(colmun_number) {
+  var sheet = SpreadsheetApp.getActiveSheet();
+  var result = sheet.getRange(1, colmun_number);
+  result = result.getA1Notation();
+  result = result.replace(/\d/,'');
+  return result;
+}
